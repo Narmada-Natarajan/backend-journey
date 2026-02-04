be7cd53ed8cc6038c584a6af4f9a8a9063170abd
Narmada-Natarajan <narmadanatarajan3629@gmail.com>
2026-02-04 08:23:45 +0530

    [completed]:basic mark and view attendance of student

--- Student Attendance Management System/controllers/attendance.contollers.js
+++ Student Attendance Management System/controllers/attendance.contollers.js
@@ -3,24 +3,50 @@ import Students from "../models/student.models.js"
 
 
 
-//mark attendance
+//Mark Attendance (Teacher)
+
+import Attendance from "../models/attendance.model.js"
 
 export const markAttendance = async (req, res) => {
 
-    let { studentId, date, status } = req.body
+  const { studentId, date, status } = req.body
 
-    if (!studentId || !date || !status) {
-        return res.status(400).json({ status: false, message: "Invalid Request" })
-    }
+  if (!studentId || !date || !status) {
+    return res.status(400).json({ status: false, message: "Invalid Request" })
+  }
 
-    try {
-        const student = await Students.findOne({})
-    }
-    catch(error){
+  try {
+    await Attendance.create({ studentId, date, status })
+    res.json({ status: true, message: "Attendance marked" })
 
-    }
-
+  } catch (error) {
+    res.status(409).json({
+      status: false,
+      message: "Attendance already marked for this date"
+    })
+  }
 }
 
 
+
+// View Attendance (Student)
+
+export const viewAttendancePercentage = async (req, res) => {
+
+  const { studentId } = req.body
+
+  const records = await Attendance.find({ studentId })
+
+  if (records.length === 0) {
+    return res.json({ status: false, message: "No records found" })
+  }
+
+  const present = records.filter(r => r.status === "present").length
+  const percentage = (present / records.length) * 100
+
+  res.json({
+    status: true,
+    attendancePercentage: percentage.toFixed(2) + "%"
+  })
+}
 
--- Student Attendance Management System/server.js
+++ Student Attendance Management System/server.js
@@ -1,8 +1,8 @@
 import express from "express"; 
 import { connectDB } from "./config/db.config.js";
 import { userLogin, userRegister } from "./controllers/user.controllers.js";
-import { authTeacher } from "./middlewares/role.middlewares.js";
-import { markAttendance } from "./controllers/attendance.contollers.js";
+import { authStudent, authTeacher } from "./middlewares/role.middlewares.js";
+import { markAttendance, viewAttendancePercentage } from "./controllers/attendance.contollers.js";
 import { authmiddleware } from "./middlewares/auth.middlewares.js";
 
 const server=express()
@@ -19,6 +19,10 @@ server.post("/register",userRegister)
 server.post("/login",userLogin)
 
 server.post("/mark-attendance",authmiddleware,authTeacher,markAttendance)
+
+server.get("/view-attendance",authmiddleware,authStudent,viewAttendancePercentage)
+
+
 
 
 
